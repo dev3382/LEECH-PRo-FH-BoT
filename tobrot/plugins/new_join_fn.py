4595f0de77d0a870e155036c570f26755f606c6d
LionKettyUD <88967484+LionKettyUD@users.noreply.github.com>
2021-10-16 23:54:36 +0530

    Add files via upload

---
+++ tobrot/plugins/new_join_fn.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+import pyrogram
+
+
+from tobrot import (
+    AUTH_CHANNEL
+)
+
+
+async def new_join_f(client, message):
+    chat_type = message.chat.type
+    if chat_type != "private":
+        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
+        # leave chat
+        await client.leave_chat(
+            chat_id=message.chat.id,
+            delete=True
+        )
+    # delete all other messages, except for AUTH_CHANNEL
+    await message.delete(revoke=True)
+
+
+async def help_message_f(client, message):
+    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    #channel_id = str(AUTH_CHANNEL)[4:]
+    #message_id = 99
+    # display the /help
+    
+    await message.reply_text(""" """, disable_web_page_preview=True)
+
+
+#async def rename_message_f(client, message):
+#  inline_keyboard = []
+#    inline_keyboard.append([
+#        pyrogram.InlineKeyboardButton(
+#            text="👉🏻 Click Here 👈🏻",
+#            url="https://t.me/MeGBots/2"
+#        )
+#    ])
+#    reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
+#    await message.reply_text(
+#        "<b>Hey Dude !!!</b>\n<b>📡 Need Bot To Renamer Bot..???</b>\n\n<b>😌 Then Click On Below Button To See it</b>",
+#        quote=True,
+#        parse_mode="html",
+#        reply_markup=reply_markup
+#    )
